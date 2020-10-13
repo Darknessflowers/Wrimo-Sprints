2d2f6c5c6e296c68a7943f646e53a99dc2daeca7
Darknessflowers <darknessflowers@hotmail.co.uk>
2020-10-13 16:58:29 +1300

    Refine timer

--- scripts.js
+++ scripts.js
@@ -3,20 +3,15 @@
 // let twentyMinData = [];
 // let thirtyMinData = [];
 // let customMindata = [];
-// let timerDuration = 0;
 let timeLeft = 0;
-let timeLeftSeconds = 0;
-// let elapsedTime = 0;
-// let currentTime = 0;
 let timerForm = document.querySelector('form');
 let timerValue = document.querySelector("input[name='sprint-length']:checked");
+let timeLeftSeconds = timerValue.value * 60;
 let timerDisplay = document.querySelector('.timeDisplay');
-// let timerSound = '';
 let startNextMinTimer;
 let startTimerBtn = document.querySelector('#startNow');
 let startNextBtn =  document.querySelector('#startNext');
 var timer;
-// let timeNow;
 function startTimer(seconds) {
   // debugger;
   let ms = seconds * 1000;
@@ -30,11 +25,7 @@ function startTimer(seconds) {
       console.log('time is up');
       clearInterval(timer);
     }
-    // timeLeft = timeLeft - 1;
-    // updateDisplay();
-    // currentTime = new Date().getTime();
-    // elapsedTime = ;
-  }, 1000);
+  }, 250);
 }
 
 function pauseTimer() {
@@ -56,41 +47,71 @@ function startNextMin() {
         console.log('Starting timer!');
         clearInterval(startNextMinTimer);
         startTimer(timeLeftSeconds);
-        
     }
   }, 1000);
 }
 
-function updateDisplay() {
-  // debugger;
-  // const msLeft = timeLeft;
-  const secondsLeft = timeLeftSeconds;
-  let displayResult = '';
-  const seconds = secondsLeft % 60;
-  const minutes = parseInt((secondsLeft/60) % 60);
-  let hours = parseInt(secondsLeft/3600);
-    //add zeros if less than ten
-    function addLeadingZeros(time) {
-      return time < 10 ? `0${time}` : time;
-    }
-  if(hours > 0) {
-    displayResult += `${hours}:`;
+// function updateDisplayBeforeStart() {
+//   // debugger;
+//   // const msLeft = timeLeft;
+//   const secondsLeft = timeLeftSeconds;
+//   let displayResult = '';
+//   const seconds = secondsLeft % 60;
+//   const minutes = parseInt((secondsLeft/60) % 60);
+//   let hours = parseInt(secondsLeft/3600);
+//     //add zeros if less than ten
+//     function addLeadingZeros(time) {
+//       return time < 10 ? `0${time}` : time;
+//     }
+//   if(hours > 0) {
+//     displayResult += `${hours}:`;
+//   }
+//     displayResult += `${addLeadingZeros(minutes)}:${addLeadingZeros(seconds)}`;
+//     timerDisplay.innerText = displayResult;
+// }
+
+function updateDisplayBeforeStart() {
+  switch(timerValue.value) {
+    case '1':
+      timerDisplay.innerText = "01:00";
+    break;
+    case '10':
+      timerDisplay.innerText = "10:00";
+    break;
+    case '15':
+      timerDisplay.innerText = "15:00";
+    break;
+    case '20':
+      timerDisplay.innerText = "20:00";
+    break;
+    case '30':
+      timerDisplay.innerText = "30:00";
+    break;
+    default:
+      timerDisplay.innerText = "00:00";
+    break;
   }
-    displayResult += `${addLeadingZeros(minutes)}:${addLeadingZeros(seconds)}`;
-    timerDisplay.innerText = displayResult;
 }
+
+function updateDisplay() {
+  let min = Math.floor(timeLeft/60000);
+  let seconds = Math.floor(timeLeft/1000)%60;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+  timerDisplay.innerHTML = min+":"+seconds;
+}
+
+updateDisplayBeforeStart();
 
 timerForm.addEventListener('change', function() {
   // update timer value when radio button is selected
   timerValue = document.querySelector("input[name='sprint-length']:checked");
   console.log(timerValue.value);
-  //convert min to ms
-  // debugger;
+  //convert min to seconds
   timeLeftSeconds = timerValue.value * 60;
-  updateDisplay();
+  updateDisplayBeforeStart();
 });
 
-startTimerBtn.addEventListener('click', function(e) {
+startTimerBtn.addEventListener('click', function() {
   startTimer(timeLeftSeconds);
 });
 startNextBtn.addEventListener('click', startNextMin);
